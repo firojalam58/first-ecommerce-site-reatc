0925dd2eaf9cbda01f02bf9a8fc2a99723b4a36d
Firoj Alam Raju <firojalamraju85@gmail.com>
2022-09-26 19:48:18 +0600

    cart added

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -29,7 +30,7 @@ const Shop = () => {
                }
             </div>
             <div className="cart-container">
-                <h2>Total Item Added: {cart.length}</h2>
+                <Cart cart={cart}></Cart>
                 
             </div>
         </div>
